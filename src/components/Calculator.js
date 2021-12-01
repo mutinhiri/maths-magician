caef4d855f25784922d7c8656838698921537c97
mutinhiri <barnabmutinhiri@gmail.com>
2021-12-01 08:56:51 +0200

    Add operator button

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -12,6 +12,13 @@ class Calculator extends React.Component {
         </div>
         <button type= 'button'> AC</button>
         <button type= 'button'>+/-</button>
+        <button type= 'button'>%</button>
+        <button type ="button" className="operator-button">÷</button>
+        <button type= "button">7</button>
+        <button type="button">8</button>
+        <button type= "button">9</button>
+        <button type= "button" className="operator-button">x</button>
+        <button type= 'button'>4</button>
       </div>
     )
   }
