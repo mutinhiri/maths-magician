68edd246fbd2d10de422697f9fae4a8cac5342e5
mutinhiri <barnabmutinhiri@gmail.com>
2021-12-02 12:48:42 +0200

    Add default functionality to calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -36,25 +36,18 @@ class Calculator extends React.Component {
           {data.next || data.operation || data.total || 0}
 
         </div>
-        <button type="button"> AC</button>
-        <button type="button">+/-</button>
-        <button type="button">%</button>
-        <button type="button" className="operator-button">÷</button>
-        <button type="button">7</button>
-        <button type="button">8</button>
-        <button type="button">9</button>
-        <button type="button" className="operator-button">x</button>
-        <button type="button">4</button>
-        <button type="button">5</button>
-        <button type="button">6</button>
-        <button type="button" className="operator-button">-</button>
-        <button type="button">1</button>
-        <button type="button">2</button>
-        <button type="button">3</button>
-        <button type="button" className="operator-button">+</button>
-        <button type="button" className="double">0</button>
-        <button type="button">.</button>
-        <button className="operator-button" type="button">=</button>
+       {
+         buttons.map((btn)=> (
+           <Button key={btn.id} className={
+            btn === '÷' || btn === 'x' || btn === '-' || btn === '+' || btn === '='
+              ? 'operation-btn'
+              : `btn${btn}`
+          }
+          data={data}
+          buttonClick={() => this.buttonClick(btn)}
+          name={btn}/>
+         ))
+       }
       </div>
     );
   }
