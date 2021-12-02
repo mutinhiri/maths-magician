1bb31ebbd35795a0ac47ffb5915d097660dfd843
mutinhiri <barnabmutinhiri@gmail.com>
2021-12-02 12:30:58 +0200

    Add button Components

---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from 'prop-types'
+
+function Button(props){
+  const { name, className, buttonClick } = props
+  return(
+    <button onClick = {buttonClick} className = {className} type="button">{name}</button>
+  )
+}
+
