332fe559e5346bdd39b571de274cdf2b177a0f67
mutinhiri <barnabmutinhiri@gmail.com>
2021-12-08 09:24:58 +0200

    Add changes to Route elements

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ import './App.css';
 
 const App = () => (
   <Router>
-    <header className='header'>
+    <header className="header">
       <h1>Math Magician</h1>
-      <ul className='navigation'>
+      <ul className="navigation">
         <li><a href="home">Home</a></li>
         <li><a href="calculator">Calculator</a></li>
         <li><a href="quote">Quote</a></li>
@@ -19,9 +19,9 @@ const App = () => (
     </header>
     <div className="App">
       <Routes>
-        <Route path='/' element={<h2> Homepage</h2>} />
-        <Route path='/calculator' element = {<Calculator/>} />
-        <Route path='/quote' element={<h1>Welcome to the quote page</h1>}/>
+        <Route path="/" element={<h2> Homepage</h2>} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<h1>Welcome to the quote page</h1>} />
       </Routes>
     </div>
 
